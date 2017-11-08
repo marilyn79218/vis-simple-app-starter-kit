456303b17c65e9bae70066ce3e2b05a1108c99ad
WendellLiu <wendell.liu@viscovery.com>
2017-11-08 16:12:41 +0800

    webpack config for css module

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -159,7 +159,7 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.m.css$/,
+            test: /\.m\.css$/,
             use: [
               require.resolve('style-loader'),
               {
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -166,7 +166,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.m.css$/,
+            test: /\.m\.css$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
